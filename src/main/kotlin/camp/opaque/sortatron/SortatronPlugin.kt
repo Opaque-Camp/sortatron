4d1df813d38407e16f80c5be96ba2dfa77bd1fb2
javabird25 <shockck84@gmail.com>
2022-11-05 19:58:24 +0500

    SortatronPlugin: extract listener registration to a method

--- src/main/kotlin/camp/opaque/sortatron/SortatronPlugin.kt
+++ src/main/kotlin/camp/opaque/sortatron/SortatronPlugin.kt
@@ -10,10 +10,12 @@ class SortatronPlugin : JavaPlugin() {
     override fun onEnable() {
         logger.info("Sortatron plugin starting up...")
         Recipes().register(server)
-        server.pluginManager.registerEvents(
-            PrepareCustomItemCraftEventListener(customItemStackMarker),
-            this
-        )
+        registerListeners()
         logger.info("Sortatron plugin startup complete.")
+    }
+
+    private fun registerListeners() {
+        server.pluginManager.registerEvents(PrepareCustomItemCraftEventListener(customItemStackMarker), this)
+        server.pluginManager.registerEvents(ConfiguratorInventoryOpenEventListener(), this)
     }
 }
