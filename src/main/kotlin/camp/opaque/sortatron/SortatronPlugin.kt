35f551b1fc2dfe83318d5ea07eebdffc4bf85525
javabird25 <shockck84@gmail.com>
2022-11-05 16:37:47 +0500

    Add SortatronTerminalRecipe

--- src/main/kotlin/camp/opaque/sortatron/SortatronPlugin.kt
+++ src/main/kotlin/camp/opaque/sortatron/SortatronPlugin.kt
@@ -3,7 +3,21 @@ package camp.opaque.sortatron
 import org.bukkit.plugin.java.JavaPlugin
 
 class SortatronPlugin : JavaPlugin() {
-    override fun onEnable() {
+    private val logger = SortatronLogger(getLogger())
+    private val itemStackFactory = ItemStackFactoryImpl()
+    private val sortatronTerminalItemStackGenerator = SortatronTerminalItemStackGeneratorImpl(itemStackFactory)
 
+    override fun onEnable() {
+        logger.info("Sortatron plugin starting up...")
+        registerRecipes()
+        logger.info("Sortatron plugin startup complete.")
+    }
+
+    private fun registerRecipes() {
+        server.addRecipe(SortatronTerminalRecipe(sortatronTerminalItemStackGenerator))
+    }
+
+    companion object {
+        const val NAMESPACE = "sortatron"
     }
 }
