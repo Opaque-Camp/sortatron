8b2ff20bc6e109c533e151d0f0d067f8d2261c7d
javabird25 <shockck84@gmail.com>
2022-11-05 15:48:29 +0500

    Add MockK dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,7 @@ dependencies {
     val kotestVersion = "5.5.4"
     testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
     testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
+    testImplementation("io.mockk:mockk:1.13.2")
 }
 
 tasks.test {
