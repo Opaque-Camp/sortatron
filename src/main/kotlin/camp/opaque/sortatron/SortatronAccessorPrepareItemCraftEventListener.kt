ad105f4bd783426156c23c60ad2a67a58147f568
javabird25 <shockck84@gmail.com>
2022-11-05 18:29:18 +0500

    Add SortatronAccessorPrepareItemCraftEventListener

---
+++ src/main/kotlin/camp/opaque/sortatron/SortatronAccessorPrepareItemCraftEventListener.kt
@@ -0,0 +1,22 @@
+package camp.opaque.sortatron
+
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.inventory.PrepareItemCraftEvent
+import org.bukkit.inventory.ShapedRecipe
+
+/**
+ * Listens for the crafting of a Sortatron Accessor and modifies the result ItemStack to have properties of
+ * the Sortatron Accessor using [SortatronAccessorItemStackGenerator].
+ */
+class SortatronAccessorPrepareItemCraftEventListener(
+    private val sortatronAccessorItemStackGenerator: SortatronAccessorItemStackGenerator
+) : Listener {
+    @EventHandler
+    fun onPrepareItemCraft(event: PrepareItemCraftEvent) {
+        val recipeKey = (event.recipe as? ShapedRecipe)?.key ?: return
+        if (recipeKey.key == CustomMaterial.SORTATRON_ACCESSOR.name.lowercase()) {
+            sortatronAccessorItemStackGenerator.modifyStack(event.inventory.result ?: return)
+        }
+    }
+}
--- src/main/kotlin/camp/opaque/sortatron/SortatronPlugin.kt
+++ src/main/kotlin/camp/opaque/sortatron/SortatronPlugin.kt
@@ -5,12 +5,17 @@ import org.bukkit.plugin.java.JavaPlugin
 class SortatronPlugin : JavaPlugin() {
     private val logger = SortatronLogger(getLogger())
     private val sortatronConfiguratorItemStackGenerator = SortatronConfiguratorItemStackGeneratorImpl()
+    private val sortatronAccessorItemStackGenerator = SortatronAccessorItemStackGeneratorImpl()
 
     override fun onEnable() {
         logger.info("Sortatron plugin starting up...")
         Recipes().register(server)
         server.pluginManager.registerEvents(
             SortatronConfiguratorPrepareItemCraftEventListener(sortatronConfiguratorItemStackGenerator),
+            this
+        )
+        server.pluginManager.registerEvents(
+            SortatronAccessorPrepareItemCraftEventListener(sortatronAccessorItemStackGenerator),
             this
         )
         logger.info("Sortatron plugin startup complete.")
